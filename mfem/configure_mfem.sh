74a0fe6e952fb223cb1e52fc4d8203de0f813b7c
Sibusiso Mabuza <ssmabuza@outlook.com>
2025-04-28 15:43:29 -0400

    updated mfem scripts

--- mfem/configure_mfem.sh
+++ mfem/configure_mfem.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
 MPI_DIR="/usr"
-MFEM_PDIR="${HOME}/Software/mfem"
+MFEM_PDIR="${HOME}/software/mfem"
 
 rm -rf CMakeCache.txt CMakeFiles 
 \
 cmake \
       -D MFEM_USE_MPI=YES \
       -D MPI_CXX=/usr/bin/mpic++ \
-      -D CMAKE_INSTALL_PREFIX="${HOME}/Software/mfem" \
+      -D CMAKE_INSTALL_PREFIX="${HOME}/software/mfem" \
       -D CMAKE_CXX_COMPILER:FILEPATH=/usr/bin/mpicxx \
       -D CMAKE_C_COMPILER:FILEPATH=/usr/bin/mpicc \
       -D HYPRE_DIR="${MFEM_PDIR}" \
       -D METIS_DIR="${MFEM_PDIR}" \
+      -D PETSC_DIR="${MFEM_PDIR}" \
+      -D PETSC_ARCH="" \
       -D LAPACK_LIBRARIES:FILEPATH="/usr/lib/x86_64-linux-gnu/liblapack.so" \
       -D BLAS_LIBRARIES:FILEPATH="/usr/lib/x86_64-linux-gnu/libblas.so" \
-      -D MFEM_USE_PETSC:BOOL=OFF \
+      -D MFEM_USE_PETSC:BOOL=ON \
       -D MFEM_USE_SUNDIALS:BOOL=ON \
 ../mfem
 
--- mfem/configure_sundials.sh
+++ mfem/configure_sundials.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-MFEM_PDIR="${HOME}/Software/mfem"
+MFEM_PDIR="${HOME}/software/mfem"
 
 rm -rf CMakeCache.txt CMakeFiles 
 \
 cmake \
       -D ENABLE_MPI=ON \
-      -D CMAKE_INSTALL_PREFIX="${HOME}/Software/mfem" \
+      -D CMAKE_INSTALL_PREFIX="${HOME}/software/mfem" \
       -D CMAKE_C_COMPILER:FILEPATH=mpicc \
       -D HYPRE_INCLUDE_DIR="${MFEM_PDIR}/include" \
       -D HYPRE_LIBRARY_DIR="${MFEM_PDIR}/lib" \
